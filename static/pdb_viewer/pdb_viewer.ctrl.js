c37cbdca458ab017fe7b957fcd36472784c0935a
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-05-11 06:37:30 +0100

    Added residue selection.

--- static/pdb_viewer/pdb_viewer.ctrl.js
+++ static/pdb_viewer/pdb_viewer.ctrl.js
@@ -1,16 +1,53 @@
 pdbViewerApp.controller("pdbViewerCtrl", function($scope, $timeout) {
-	pdb = {id:"2VVM",residues:[1,5]}
+	result = {"pdb": {id:"2VVM", residues:[1,5]}}
+	mol = null;
+	selectedAtoms = [];
 	
 	load_pdb = function() {
-		pv.io.fetchPdb('http://files.rcsb.org/download/' + pdb.id + '.pdb', function(newMol) {
+		pv.io.fetchPdb("http://files.rcsb.org/download/" + result.pdb.id + ".pdb", function(newMol) {
 			mol = newMol;
-			geom = viewer.cartoon('protein', mol, {color : color.bySS()});
-			selectedAtoms = [];
+			geom = viewer.cartoon("protein", mol, {color : color.bySS()});
+			highlightResidues()
 			viewer.autoZoom();
 		});
 	}
 	
-	$scope.$watch("pdb.id", function(val) {
+	highlightResidues = function() {
+		// Revert currently selected atoms:
+		for(var i = 0; i < selectedAtoms.length; i++) {
+			setColorForAtom(selectedAtoms[i].atom, selectedAtoms[i].color);
+		}
+	
+		selectedAtoms = [];
+		
+		// Colour newly selected atoms in each chain:
+		var chains = mol.chains();
+		
+		for(var i = 0; i < chains.length; i++) {
+			var chainName = chains[i].name();
+			
+			for(var j = 0; j < result.pdb.residues.length; j++ ) {
+				var atom = mol.atom(chainName + "." + result.pdb.residues[j] + ".CA");
+				
+				if(atom !== null) {
+					var color = [0,0,0,0];
+					geom.getColorForAtom(atom, color);
+					selectedAtoms.push({atom: atom, color: color});
+					setColorForAtom(atom, "red");
+				}
+			}	
+		}
+		
+		viewer.requestRedraw();
+	}
+	
+	setColorForAtom = function(atom, color) {
+	    var view = mol.createEmptyView();
+	    view.addAtom(atom);
+	    geom.colorBy(pv.color.uniform(color), view);
+	}
+	
+	$scope.$watch("result.pdb.id", function(val) {
 		$timeout(function() {
 			load_pdb()
 		}, 0)
