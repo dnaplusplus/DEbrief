beaa74f3403ae60b809211912826122eb248fba0
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-05-11 12:52:41 +0100

    Updated mutation highlights from paginator.

--- static/debrief/debrief.ctrl.js
+++ static/debrief/debrief.ctrl.js
@@ -1,15 +1,19 @@
 debriefApp.controller("debriefCtrl", ["$scope", "$timeout", "DEBriefService", function($scope, $timeout, DEBriefService) {
 	var self = this;
-	
+
 	self.mutationsList = DEBriefService.mutationsList;
 	self.pagination = {current: 1};
-	
+
+	self.update = function() {
+		highlightMutations();
+	}
+
 	pdb = DEBriefService.pdb;
-	
+
 	mutations = function() {
 		return DEBriefService.mutationsList[self.pagination.current - 1];
 	}
-	
+
 	mol = null;
 	currentMutations = [];
 
@@ -17,49 +21,51 @@ debriefApp.controller("debriefCtrl", ["$scope", "$timeout", "DEBriefService", fu
 		pv.io.fetchPdb("http://files.rcsb.org/download/" + pdb.id + ".pdb", function(newMol) {
 			mol = newMol;
 			geom = viewer.cartoon("protein", mol, {color : color.bySS()});
-			highlightResidues()
+			highlightMutations()
 			viewer.autoZoom();
 		});
 	}
-	
+
 	highlightMutations = function() {
-		// Revert currently selected atoms:
-		for(var i = 0; i < currentMutations.length; i++) {
-			setColorForAtom(currentMutations[i].atom, currentMutations[i].color);
+		if(mol) {
+			// Revert currently selected atoms:
+			for(var i = 0; i < currentMutations.length; i++) {
+				setColorForAtom(currentMutations[i].atom, currentMutations[i].color);
+			}
+
+			currentMutations = [];
+
+			// Colour newly selected atoms in each chain:
+			var chains = mol.chains();
+
+			for(var i = 0; i < chains.length; i++) {
+				var chainName = chains[i].name();
+
+				for(var j = 0; j < mutations().length; j++ ) {
+					var atom = mol.atom(chainName + "." + mutations()[j] + ".CA");
+
+					if(atom !== null) {
+						var color = [0,0,0,0];
+						geom.getColorForAtom(atom, color);
+						currentMutations.push({atom: atom, color: color});
+						setColorForAtom(atom, "red");
+					}
+				}	
+			}
+
+			viewer.requestRedraw();
 		}
-	
-		currentMutations = [];
-		
-		// Colour newly selected atoms in each chain:
-		var chains = mol.chains();
-		
-		for(var i = 0; i < chains.length; i++) {
-			var chainName = chains[i].name();
-			
-			for(var j = 0; j < mutations().length; j++ ) {
-				var atom = mol.atom(chainName + "." + mutations()[j] + ".CA");
-				
-				if(atom !== null) {
-					var color = [0,0,0,0];
-					geom.getColorForAtom(atom, color);
-					currentMutations.push({atom: atom, color: color});
-					setColorForAtom(atom, "red");
-				}
-			}	
-		}
-		
-		viewer.requestRedraw();
 	}
-	
+
 	setColorForAtom = function(atom, color) {
-	    var view = mol.createEmptyView();
-	    view.addAtom(atom);
-	    geom.colorBy(pv.color.uniform(color), view);
+		var view = mol.createEmptyView();
+		view.addAtom(atom);
+		geom.colorBy(pv.color.uniform(color), view);
 	}
-	
+
 	$scope.$watch("pdb.id", function(val) {
 		$timeout(function() {
-			load_pdb()
+			load_pdb();
 		}, 0)
 	});
 }]);
