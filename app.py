367a8d761b47bdf3e7368ad3831d0a95a68f8ddf
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-06-12 15:48:43 +0100

    Fixed authentication issues.

--- app.py
+++ app.py
@@ -1,16 +1,136 @@
 '''
-PathwayGenie (c) University of Manchester 2017
+DEbrief (c) University of Manchester 2017
 
-PathwayGenie is licensed under the MIT License.
+DEbrief is licensed under the MIT License.
 
 To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 
 @author:  neilswainston
 '''
+import StringIO
+import json
 import sys
 import uuid
 
-from debrief import APP
+from Bio import Seq, SeqIO, SeqRecord
+from apiclient import discovery
+from oauth2client import client
+from oauth2client.file import Storage
+import flask
+import httplib2
+
+from debrief.debrief_db import DEBriefDBClient
+
+APP = flask.Flask(__name__)
+
+
+@APP.route('/')
+def index():
+    '''Renders homepage.'''
+    credentials = _get_credentials()
+
+    if not credentials:
+        return flask.redirect(flask.url_for('oauth2callback'))
+    elif credentials.access_token_expired:
+        return flask.redirect(flask.url_for('oauth2callback'))
+
+    return APP.send_static_file('index.html')
+
+
+@APP.route('/oauth2callback')
+def oauth2callback():
+    '''Callback method.'''
+    flow = client.flow_from_clientsecrets(
+        'client_id.json',
+        scope='https://www.googleapis.com/auth/drive',
+        redirect_uri=flask.url_for('oauth2callback', _external=True))
+
+    flow.params['include_granted_scopes'] = 'true'
+
+    if 'code' not in flask.request.args:
+        auth_uri = flow.step1_get_authorize_url()
+        return flask.redirect(auth_uri)
+    else:
+        auth_code = flask.request.args.get('code')
+        credentials = flow.step2_exchange(auth_code)
+
+    # write access token to credentials.json locally
+    open('credentials.json', 'w').write(credentials.to_json())
+    return flask.redirect(flask.url_for('index'))
+
+
+@APP.route('/data/<project_id>')
+def get_data(project_id):
+    '''Gets a pdb id and mutations from a project id.'''
+    debrief = _get_debrief(project_id)
+    result = {'pdb': {'id': debrief.get_pdb_id()},
+              'mutations': debrief.get_mutations().values()}
+
+    return flask.Response(json.dumps(result, indent=3, sort_keys=True),
+                          mimetype='application/json')
+
+
+@APP.route('/fasta/<project_id>')
+def get_fasta(project_id):
+    '''Gets a fasta file from a project id.'''
+    debrief = _get_debrief(project_id)
+    records = [SeqRecord.SeqRecord(Seq.Seq(seq), id=seq_id, name='',
+                                   description='')
+               for seq_id, seq in debrief.get_sequences().iteritems()]
+
+    result = StringIO.StringIO()
+    SeqIO.write(records, result, 'fasta')
+
+    response = flask.Response(result.getvalue(), mimetype='application/text')
+    response.headers['Content-Disposition'] = \
+        'attachment; filename=%s.fasta' % project_id
+    return response
+
+
+@APP.route('/md-worklist/<project_id>')
+def get_md_worklist(project_id):
+    '''Gets a molecular dynamics worklist from a project id.'''
+    debrief = _get_debrief(project_id)
+    resp = '\n'.join(debrief.get_md_worklist())
+
+    response = flask.Response(resp, mimetype='application/text')
+    response.headers['Content-Disposition'] = \
+        'attachment; filename=%s_worklist.txt' % project_id
+    return response
+
+
+def _get_debrief(project_id):
+    '''Gets values.'''
+    serv = _get_service()
+    values = serv.spreadsheets().values().get(
+        spreadsheetId='1-dcR5dPaYwtH38HNYqBieOSaqMz-31N8aEdEb3IqRkw',
+        range=project_id + '!' + 'A:R').execute()
+
+    return DEBriefDBClient(values.get('values', []))
+
+
+def _get_service():
+    '''Gets service.'''
+    credentials = _get_credentials()
+
+    if not credentials or credentials.access_token_expired:
+        return flask.redirect(flask.url_for('oauth2callback'))
+
+    credentials = _get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    url = ('https://sheets.googleapis.com/$discovery/rest?version=v4')
+    return discovery.build('sheets', 'v4', http=http, discoveryServiceUrl=url)
+
+
+def _get_credentials():
+    '''Gets credentials.'''
+    store = Storage('credentials.json')
+    credentials = store.get()
+
+    if not credentials or credentials.invalid:
+        return None
+
+    return credentials
 
 
 def main(argv):
--- debrief/debrief_db.py
+++ debrief/debrief_db.py
@@ -10,7 +10,6 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 from collections import defaultdict
 from operator import itemgetter
 import re
-import sys
 
 
 class DEBriefDBClient(object):
